d0a5e3fb510ec7d5591371c1d7503433b399bd17
jshuckbot <gorka_ivan@mail.ru>
2024-02-25 00:29:41 +0700

    edit docker-compose

--- coursesapp/views.py
+++ coursesapp/views.py
@@ -7,8 +7,8 @@ from coursesapp.models import Content, Module
 
 def index(request):
     module = Module.objects.get(pk=8)
-    obj = ContentType.objects.get_for_model(module)
+    # obj = ContentType.objects.get_for_model(module)
     # obj = ContentType.objects.get(app_label='coursesapp', model='module')
-    lst = Content.objects.filter(content_type__pk=obj.pk, object_id=module.pk)
-    print(lst)
+    # lst = Content.objects.filter(content_type__pk=obj.pk, object_id=module.pk)
+    # print(lst)
     return HttpResponse("Hello")
